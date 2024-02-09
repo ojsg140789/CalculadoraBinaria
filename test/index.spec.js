d7ca489da514893fdfadd296f9032a92d6325503
Omar Soto <ojsg140789@gmail.com>
2024-02-09 17:18:10 -0600

    fix: Se corrige error de sintaxis de expect

--- test/index.spec.js
+++ test/index.spec.js
@@ -21,11 +21,11 @@ describe('CalculadoraBinaria', () => {
   //Construir test
   it('Prueba 1: Input binario 11001 a decimal igual a 25', () => {
     let resultado = app.convertirBinarioDecimal('11001');
-    expect( resultado, '25')
+    expect(resultado).to.equal(25);
   });
 
   it('Prueba 2: Input binario incorrecto 11?001 a decimal igual a null', () => {
     let resultado = app.convertirBinarioDecimal('11?001');
-    expect( resultado, null)
+    expect(resultado).to.equal(null);
   });
 });
